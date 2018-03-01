6fe1e81d7691276785a3a27b1d7898bfc6a72107
Zach Stearman <zstearman@prophetratings.com>
2018-03-01 17:43:32 +0000

    Add helper to simplify titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Prophet Ratings - NCAA Basketball Predictive Analytics"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
