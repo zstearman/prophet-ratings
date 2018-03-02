dbc718b570081d3c9a34a53dbc53a1f1cb6dcb8c
Zach Stearman <zstearman@prophetratings.com>
2018-03-02 17:59:58 +0000

    Add some info on about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,19 +7,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Prophet Ratings - NCAA Basketball Predictive Analytics"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Prophet Ratings - NCAA Basketball Predictive Analytics"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Prophet Ratings - NCAA Basketball Predictive Analytics"
   end
