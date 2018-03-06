c8b66cc60f49039f7831961cb73c1944c8fa3b88
Zach Stearman <zstearman@prophetratings.com>
2018-03-06 16:51:59 +0000

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/plans',   to: 'static_pages#plan'
-  get '/signup',  to: 'users#new'
-  post '/signup', to: 'users#create'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/plans',   to: 'static_pages#plan'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
