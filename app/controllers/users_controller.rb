5a025d56cf27ed5b427aded055bdb44f96146f87
Zach Stearman <zstearman@prophetratings.com>
2018-03-06 04:12:06 +0000

    Add strong params on user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(user_params.merge(:expiration_date => Date.today,
+                                       :account_level => params[:plan_id]))
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email,
+                                   :password, :password_confirmation,
+                                   :account_level, :expiration_date)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: {minimum: 6, maximum: 50}
+  validates :expiration_date, presence: true
 end
