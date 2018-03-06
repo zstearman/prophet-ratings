67ce8f84bdddc61336ce93580103a71661b48a5d
Zach Stearman <zstearman@prophetratings.com>
2018-03-06 04:18:33 +0000

    Add test to invalid signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    get signup_path(:plan_id => "1")
+    assert_no_difference "User.count" do
+      post users_path, params: { user: { first_name: "",
+                                         last_name: "",
+                                         email: "user@invalid",
+                                         account_level: "1",
+                                         expiration_date: "31/08/2019",
+                                         password: "foo",
+                                         password_confirmation: "bar"} }
+    end
+  assert_template 'users/new'
+  end
+
+end
