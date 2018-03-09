3190db4fccac2fd7704eecc6f6eb8dc13a9342ee
Zach Stearman <zstearman@prophetratings.com>
2018-03-09 15:55:41 +0000

    Fix login path

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,22 +17,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
   
-  # Login not working because we are testing users controller?
-  # test "should redirect edit when logged in as wrong user" do
-  #   log_in_as(@other_user)
-  #   assert_redirected_to @other_user
-  #   get edit_user_path(@user)
-  #   #assert flash.empty?
-  #   assert_redirected_to root_url
-  # end
+  test "again login with valid information" do
+    log_in_as(@user)
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+  end
   
-  # test "should redirect update when logged in as wrong user" do
-  #   post login_path, params: { session: { email: @other_user.email,
-  #                                         password: 'password' } }
-  #   assert_redirected_to @other_user                                      
-  #   patch user_path(@user), params: { user: { first_name: @user.first_name,
-  #                                             email: @user.email } }
-  #   #assert flash.empty?
-  #   assert_redirected_to root_url
-  # end
+  # Login not working because we are testing users controller?
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    assert_redirected_to @other_user
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  test "should redirect update when logged in as wrong user" do
+    post login_path, params: { session: { email: @other_user.email,
+                                          password: 'password' } }
+    assert_redirected_to @other_user                                      
+    patch user_path(@user), params: { user: { first_name: @user.first_name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,8 +23,9 @@ class ActionDispatch::IntegrationTest
   
   # Log in as a particular user.
   def log_in_as(user, password: 'password', remember_me: '1')
+    get login_path
     post login_path, params: { session: { email: user.email,
-                                          password: password,
-                                          remember_me: remember_me } }
+                                           password: password,
+                                           remember_me: remember_me } }
   end
 end
