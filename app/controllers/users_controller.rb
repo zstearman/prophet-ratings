b05bfff65c311b616ba350c36e326ed8ab120963
Zach Stearman <zstearman@prophetratings.com>
2018-03-09 06:29:36 +0000

    Fix admin user permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :show]
-  before_action :correct_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:show]
   before_action :correct_or_admin, only: [:edit, :update] 
   before_action :admin_user, only: [:index, :destroy]
   
