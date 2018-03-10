ba437c9e8c02f6c365c798195557aeb52b7e192f
Zach Stearman <zstearman@prophetratings.com>
2018-03-10 15:57:26 +0000

    Create password resets controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,17 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now  
   end
   
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
   private
     
     def downcase_email
