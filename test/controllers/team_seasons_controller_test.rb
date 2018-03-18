b4bbba5897f0e3d7c9e4cdc3e1991cf7b0f107b5
Zach Stearman <zstearman@prophetratings.com>
2018-03-18 14:12:22 +0000

    Update fixtures

--- test/controllers/team_seasons_controller_test.rb
+++ test/controllers/team_seasons_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TeamSeasonsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get team_seasons_index_url
+    get rankings_path
     assert_response :success
   end
 
