988f4244ab7fdade92a3d005fe3de2b3fd5c3f25
Zach Stearman <zstearman@prophetratings.com>
2018-03-19 16:52:14 +0000

    Add edit link to team page

--- test/controllers/conferences_controller_test.rb
+++ test/controllers/conferences_controller_test.rb
@@ -2,12 +2,7 @@ require 'test_helper'
 
 class ConferencesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get conferences_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get conferences_show_url
+    get conferences_path
     assert_response :success
   end
 
