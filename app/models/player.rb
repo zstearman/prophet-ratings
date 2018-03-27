a3681e35e7fadeea03621d257b101391c5ed1a8a
Zach Stearman <zstearman@prophetratings.com>
2018-03-27 19:58:25 +0000

    Format height for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,10 @@
 class Player < ApplicationRecord
   has_many :player_games
   belongs_to :team
+  
+  def formatted_height
+    feet = (height / 12).floor
+    inches = height % 12
+    @formatted_height = feet.to_s + "' " + inches.to_s + '"'
+  end
 end
