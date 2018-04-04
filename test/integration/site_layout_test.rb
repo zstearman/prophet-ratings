1d827b72b631b8716659bc8083892086f190204e
Zach Stearman <zstearman@prophetratings.com>
2018-04-04 19:46:18 +0000

    Write passing tests"

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,15 +2,22 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:zach)
+  end
+  
   test "layout links" do
+    log_in_as(@user)
     get root_path
-    assert_template 'static_pages/home'
+    follow_redirect!
+    assert_template 'static_pages/dashboard'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path + "?plan_id=1"
-    assert_select "a[href=?]", signup_path + "?plan_id=2"    
-    assert_select "a[href=?]", plans_path
+    # Not functional while coming soon page is present
+    # assert_select "a[href=?]", signup_path + "?plan_id=1"
+    # assert_select "a[href=?]", signup_path + "?plan_id=2"    
+    # assert_select "a[href=?]", plans_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
